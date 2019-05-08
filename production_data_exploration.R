82338a32c2feaca5b76271c4add6be71d63500ca
Javier Atalah <jatalah@gmail.com>
2019-05-08 18:56:44 +0200

    Edits
    Edits

--- production_data_exploration.R
+++
@@ -1,7 +0,0 @@
-prod_data_all %>%
-  filter(Major_Group == "PISCES") %>%
-  filter(YEAR > 2015) %>% 
-  group_by(Scientific_Name) %>%
-  summarise(Q = sum(QUANTITY)) %>%
-  arrange(desc(Q)) %>%
-  full_join(spp, by = "Scientific_Name")
